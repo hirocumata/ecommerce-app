d4e0f818c0d1ef3256be3972bd44c57afb0e4f81
hirocumata <132476660+hirocumata@users.noreply.github.com>
2023-05-03 18:06:54 +0100

    Add files via upload

--- main.go
+++ main.go
@@ -1,2 +1,33 @@
 package main
 
+import (
+	
+	"github.com/hirocumata/ecommerce-app/controllers"
+	"github.com/hirocumata/ecommerce-app/database"
+	"github.com/hirocumata/ecommerce-app/middleware"
+	"github.com/hirocumata/ecommerce-app/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os - Getenv("PORT")
+	if port == "" {
+		port = 8080
+	}
+
+	app := controllers.NewApplication(database.ProductData(database.client, "Products"), database.UserData(database.Client, "Users"))
+
+	router := gin.New()
+	router.Use(Gin.Logger())
+
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("/removeitem", app.RemoveItem())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("instantbuy", app.InstantBuy())
+
+	log.Fatal(router.Run(":" + port))
+
+}
